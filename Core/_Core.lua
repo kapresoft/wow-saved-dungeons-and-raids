476ad8676a280cbe4e96d8e1026de71532f36b0d
Tony Lagnada <deloyz@gmail.com>
2022-12-10 18:14:20 -0800

    Initial checkin

---
+++ Core/_Core.lua
@@ -0,0 +1,7 @@
+-- ## External -------------------------------------------------
+local format = string.format
+---@class LibStub Ace3 LibStub
+local LibStub = LibStub
+
+local addon, ns = ...
+print('loaded:', addon)
