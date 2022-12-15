81cc66fad42fcf2aacc00b6de100503b456a3d52
Tony Lagnada <deloyz@gmail.com>
2022-12-14 20:12:56 -0800

    update release and deployment scripts, switch to auto changelog

--- dev/script-functions.sh
+++ dev/script-functions.sh
@@ -8,12 +8,10 @@ Print "Sourcing" "${this_file}"
 # --------------------------------------------
 # vars
 # --------------------------------------------
-ADDON_NAME="SavedDungeonsAndRaids"
-proj_dir=ADDON_NAME
+proj_dir="SavedDungeonsAndRaids"
 pre_release_dir="$HOME/.wow-pre-release"
 dev_release_dir="$HOME/.wow-dev"
-EXTLIB="Core/ExtLib"
-INTERFACE_LIB="Core/Interface"
+release_script="./dev/release.sh"
 
 #KAPRESOFT_LIB="$HOME/sandbox/github/kapresoft/wow/wow-lib-util/"
 #PROJ_CORE_EXTLIB="./Core/ExtLib"
