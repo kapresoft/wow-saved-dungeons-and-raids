37bf0100c29522e303b9d871598becf2f0df6a42
Tony Lagnada <deloyz@gmail.com>
2022-12-26 17:07:11 -0800

    Bindings.xml file and skeleton config dialog options

---
+++ Core/Lib/Locales/Localization.lua
@@ -0,0 +1,11 @@
+local addonName = ...
+local L = LibStub("AceLocale-3.0"):GetLocale(addonName)
+
+-- General
+SDNR_TITLE =  addonName
+SDNR_CATEGORY                             = "AddOns/" .. SDNR_TITLE
+
+-- Key binding localization text
+BINDING_HEADER_SDNR_OPTIONS        = SDNR_TITLE
+BINDING_NAME_SDNR_OPTIONS_DLG      = L["BINDING_NAME_SDNR_OPTIONS_DLG"]
+
