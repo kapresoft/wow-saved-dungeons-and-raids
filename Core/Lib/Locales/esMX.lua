c8189d783159d631f33cef9ce78df5a392f56016
Tony Lagnada <deloyz@gmail.com>
2023-04-14 12:17:12 -0700

    Locales: es-ES and es-MX #29

---
+++ Core/Lib/Locales/esMX.lua
@@ -0,0 +1,11 @@
+local addonName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "esMX");
+if not L then return end
+
+--[[-----------------------------------------------------------------------------
+Keybinding Localization
+The contents below this block will be generated automatically.
+-------------------------------------------------------------------------------]]
+
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="concat", same-key-is-true="true" , escape-non-ascii="true")@
+
